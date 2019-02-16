dcf0680db4a396ec0ec729438039c3455ee9c2d4
Joshua Thatcher <jjthatch@iu.edu>
2019-02-15 23:57:38 -0500

    Queue Skeleton. Need Calcium.

--- Queue.cpp
+++ Queue.cpp
@@ -1,1 +1,17 @@
 //I pledge to not help or receive help from others during this assignment
+Queue (void);
+
+template <typename T>
+void Queue<T>::enqueue(T element);
+
+template <typename T>
+void Queue<T>::dequeue(void);
+
+template <typename T>
+bool Queue<T>::is_empty(void);
+
+template <typename T>
+int size(void);
+
+template <typename T>
+void clear(void);
--- Queue.h
+++ Queue.h
@@ -1,1 +1,30 @@
 // I pledge not to help or receive help from people during assignment
+
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+#include "Array.h"
+#include "Queue.cpp"
+
+template <typename T>
+class Queue : public Array <T>
+{
+public:
+  // Default constructor
+  Queue (void);
+
+  void enqueue(T element);
+
+  void dequeue(void);
+
+  bool is_empty(void);
+
+  int size(void);
+
+  void clear(void);
+
+}
+
+
+
+#endif
