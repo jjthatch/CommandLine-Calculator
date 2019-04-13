e679704decc23b8b7e19de36a59259410618f78e
Joshua Thatcher <jjthatch@iu.edu>
2019-04-12 20:42:51 -0400

    Includes compiling on mac

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,8 @@
 // Container structures
-//#include "Stack.h"
-//#include "Queue.h"
+#include "Stack.h"
+#include "Queue.h"
 
 // Composite Pattern Objects
-/*
 #include "Division_Node.h"
 #include "Addition_Node.h"
 #include "Multiplication_Node.h"
@@ -11,7 +10,7 @@
 #include "Modulus_Node.h"
 #include "Binary_Node.h"
 #include "Expr_Node.h"
-*/
+
 
 // Visitor Pattern Objects
 
